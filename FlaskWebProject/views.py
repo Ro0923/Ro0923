8297a4575937d556d687ae4b5b272fe9b6c2fc54
Ro0923 <147279745+Ro0923@users.noreply.github.com>
2026-04-27 04:23:20 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,7 +67,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
 
-        if user is None or not user.check_password(form.password.data):
+        if user is None or user.password != form.password.data:
             app.logger.warning("Invalid login attempt")
             flash('Invalid username or password')
             return redirect(url_for('login'))
