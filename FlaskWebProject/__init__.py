ad423bb4e96cdecd985bc441bae87cf922bb1a55
Ro0923 <147279745+Ro0923@users.noreply.github.com>
2026-04-27 03:02:37 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,23 +1,16 @@
 """
 The flask application package.
 """
-
 import logging
 from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
-
-# Fix HTTPS / proxy headers on Azure App Service
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-
 app.config.from_object(Config)
 
-# Logging setup
 app.logger.setLevel(logging.INFO)
 
 stream_handler = logging.StreamHandler()
@@ -28,10 +21,8 @@ stream_handler.setFormatter(formatter)
 
 app.logger.addHandler(stream_handler)
 
-# Extensions
 Session(app)
 db = SQLAlchemy(app)
-
 login = LoginManager(app)
 login.login_view = 'login'
 
