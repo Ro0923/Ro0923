7211dfd44559b0d9ed524c48c7b0a871b007f756
Ro0923 <147279745+Ro0923@users.noreply.github.com>
2026-04-27 04:33:53 +0530

    Configure logging for the Flask application
    Added logging configuration to the Flask app.

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,6 +1,7 @@
 """
 The flask application package.
 """
+
 import logging
 from flask import Flask
 from config import Config
@@ -11,18 +12,20 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Logging
+if not app.logger.handlers:
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
 app.logger.setLevel(logging.INFO)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(formatter)
-
-app.logger.addHandler(stream_handler)
-
+# Extensions
 Session(app)
 db = SQLAlchemy(app)
+
 login = LoginManager(app)
 login.login_view = 'login'
 
