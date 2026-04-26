df609691b3c01480af7716d066bf81639fe5a49b
Ro0923 <147279745+Ro0923@users.noreply.github.com>
2026-04-27 04:34:57 +0530

    Refactor render_template calls for consistency

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -2,11 +2,10 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
 from flask import render_template, flash, redirect, request, session, url_for
 from werkzeug.urls import url_parse
 from config import Config
-from FlaskWebProject import app, db
+from FlaskWebProject import app
 from FlaskWebProject.forms import LoginForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
@@ -19,13 +18,8 @@ imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + '.blob.core.windows.n
 @app.route('/home')
 @login_required
 def home():
-    user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
-    return render_template(
-        'index.html',
-        title='Home Page',
-        posts=posts
-    )
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/new_post', methods=['GET', 'POST'])
 @login_required
@@ -35,27 +29,26 @@ def new_post():
         post = Post()
         post.save_changes(form, request.files['image_path'], current_user.id, new=True)
         return redirect(url_for('home'))
-    return render_template(
-        'post.html',
-        title='Create Post',
-        imageSource=imageSourceUrl,
-        form=form
-    )
+
+    return render_template('post.html',
+                           title='Create Post',
+                           imageSource=imageSourceUrl,
+                           form=form)
 
 @app.route('/post/<int:id>', methods=['GET', 'POST'])
 @login_required
 def post(id):
     post = Post.query.get(int(id))
     form = PostForm(formdata=request.form, obj=post)
+
     if form.validate_on_submit():
         post.save_changes(form, request.files['image_path'], current_user.id)
         return redirect(url_for('home'))
-    return render_template(
-        'post.html',
-        title='Edit Post',
-        imageSource=imageSourceUrl,
-        form=form
-    )
+
+    return render_template('post.html',
+                           title='Edit Post',
+                           imageSource=imageSourceUrl,
+                           form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -69,14 +62,13 @@ def login():
 
         if user is None or user.password != form.password.data:
             app.logger.warning("Invalid login attempt")
-            flash('Invalid username or password')
+            flash("Invalid username or password")
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
         app.logger.info("admin logged in successfully")
 
         next_page = request.args.get('next')
-
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
 
@@ -85,17 +77,15 @@ def login():
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
 
-    return render_template(
-        'login.html',
-        title='Sign In',
-        form=form,
-        auth_url=auth_url
-    )
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           auth_url=auth_url)
 
 @app.route(Config.REDIRECT_PATH)
 def authorized():
     if request.args.get('state') != session.get("state"):
-        return redirect(url_for("home"))
+        return redirect(url_for("login"))
 
     if "error" in request.args:
         return render_template("auth_error.html", result=request.args)
@@ -128,11 +118,10 @@ def logout():
 
     if session.get("user"):
         session.clear()
-
         return redirect(
             Config.AUTHORITY + "/oauth2/v2.0/logout"
             + "?post_logout_redirect_uri="
-            + url_for("login", _external=True)
+            + url_for("login", _external=True, _scheme='https')
         )
 
     return redirect(url_for('login'))
@@ -162,4 +151,4 @@ def _build_auth_url(authority=None, scopes=None, state=None):
         scopes or [],
         state=state,
         redirect_uri=url_for('authorized', _external=True, _scheme='https')
-    )   
+    )
