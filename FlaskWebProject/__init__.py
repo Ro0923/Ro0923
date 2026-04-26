28ce7262a5333597ac61c7eae44235d58e7b1be9
Ro0923 <147279745+Ro0923@users.noreply.github.com>
2026-04-27 02:12:38 +0530

    Add files via upload

---
+++ FlaskWebProject/__init__.py
@@ -0,0 +1,29 @@
+"""
+The flask application package.
+"""
+import logging
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+app.logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+
+app.logger.addHandler(stream_handler)
+
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import FlaskWebProject.views
